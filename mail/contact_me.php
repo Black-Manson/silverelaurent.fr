7f89418a66fe2d25d3c59e1eebbfc73f9b970150
Black-Manson <silvere.laurent@orange.fr>
2019-03-01 17:43:38 +0100

    changement du layout, reste pas mal de contenu à faire !

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,10 +11,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = "contact@silverelaurent.fr"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Nouveau message venant du portfolio:  $name";
+$body = "Vous avez reçu un nouveau message depuis votre portfolio.\n\n"."Voici les détails du message:\n\nNom: $name\n\nEmail: $email\n\nTéléphone: $phone\n\nMessage:\n$message";
+$header = "From: noreply@silverelaurent.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
